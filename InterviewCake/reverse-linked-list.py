6e929b60a3fd7db0bb56e51d622e815d49fd0a83
david-jarman <davidbjarman@gmail.com>
2016-09-06 21:54:48 +0000

    Added out of place version of reversing linked list

--- InterviewCake/reverse-linked-list.py
+++ InterviewCake/reverse-linked-list.py
@@ -20,6 +20,26 @@ def reverse_list_inplace(head):
         else:
             return cur
 
+def reverse_list_out_of_place(head):
+    if not head:
+        return None
+    if not head.next:
+        return head
+
+    prev = None
+    cur = head
+
+    while cur:
+        new_node = LinkedListNode(cur.value)
+        new_node.next = prev
+
+        prev = new_node
+        cur = cur.next
+
+    return prev
+
+
+
 head = LinkedListNode(1)
 head.next = LinkedListNode(2)
 head.next.next = LinkedListNode(3)
@@ -31,4 +51,10 @@ print li
 
 new_head = reverse_list_inplace(head)
 li.root = new_head
+print li
+
+new_new_head = reverse_list_out_of_place(new_head)
+li_new = LinkedList()
+li_new.root = new_new_head
+print li_new
 print li
